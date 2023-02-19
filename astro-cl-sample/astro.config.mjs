5440a302eabfb55ffab045d9c37fd3a2682b3092
loxi1 <anibal.cayetano@gmail.com>
2023-02-18 23:17:05 -0500

    18-02-2023 cambios

--- astro-cl-sample/astro.config.mjs
+++ astro-cl-sample/astro.config.mjs
@@ -12,12 +12,21 @@ import prefetch from "@astrojs/prefetch";
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
+const website = 'http://localhost:300/'
+
 // https://astro.build/config
 export default defineConfig({
+  site: website,
   output: "server",
   site: 'http://127.0.0.1:3000/',
   adapter: cloudflare({
     mode: "directory"
   }),
-  integrations: [tailwind(), prefetch(), sitemap()]
+  integrations: [
+    tailwind(), 
+    prefetch(), 
+    sitemap({
+      lastmod: new Date()
+    })
+  ]
 });
